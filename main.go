35121b7a76e69a95a473e4645dc928aae24843d5
Adam Malevski <adam.malevski@ovoko.com>
2025-05-15 20:44:24 +0300

    (ci) goreleaser fixes

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	version := "0.3.4"
+	version := "0.3.9"
 	namespace := flag.String("n", "", "Kubernetes namespace to use")
 	flag.Parse()
 	ui.StartUI(version, *namespace)
