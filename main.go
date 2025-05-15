2e6f37a2647dc217f4c4b474ca26f3e3890e3fc7
Adam Malevski <adam.malevski@ovoko.com>
2025-05-15 21:07:14 +0300

    (fixes) brew

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	version := "0.3.10"
+	version := "0.3.11"
 	namespace := flag.String("n", "", "Kubernetes namespace to use")
 	flag.Parse()
 	ui.StartUI(version, *namespace)
